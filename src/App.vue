fac4134ac03583b8d87aa93a8130c46d3f50c000
Martin Richter <karag2006@gmx.net>
2020-12-26 09:51:43 +0100

    end lesson 3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}<br>
+    <strong>Followers : </strong> {{followers}}
+    <button @click="followUser">
+      Follow
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "karag2006",
+        firstname: "Martin",
+        lastname: "Richter",
+        email: "martin.richter2006@gmail.com",
+        isAdmin: true,
+      }
+    }
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(this.user.username + ' has gained a follower!');
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return (this.user.firstname + ' ' + this.user.lastname);
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  mounted() {
+    this.followUser();
   }
 }
 </script>
@@ -21,6 +55,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
